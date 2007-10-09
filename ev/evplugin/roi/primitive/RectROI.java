1c49feaba3566a96060ae565745c262064d711ce
Johan Henriksson <mahogny@areta.org>
2007-10-09 16:29:45 +0000

    *** empty log message ***

---
+++ ev/evplugin/roi/primitive/RectROI.java
@@ -0,0 +1,25 @@
+package evplugin.roi.primitive;
+
+import evplugin.roi.ROI;
+
+import java.util.*;
+
+public class RectROI implements ROI
+	{
+	
+	
+	
+	public Vector<String> getChannels()
+		{
+		Vector<String> c=new Vector<String>();
+		
+		return c;
+		}
+	
+	//get iterator: image + line iterator or pixel iterator or entire image?
+	//which channels, frames, slices are affected?
+	
+	
+	
+	
+	}
