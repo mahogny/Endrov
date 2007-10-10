b5f26dd31be025424654726ae88026ed57d793ef
Johan Henriksson <mahogny@areta.org>
2007-10-10 08:10:13 +0000

    *** empty log message ***

--- ev/evplugin/roi/primitive/RectROI.java
+++ ev/evplugin/roi/primitive/RectROI.java
@@ -15,10 +15,26 @@ public class RectROI implements ROI
 		
 		return c;
 		}
+
+	public Vector<Integer> getFrames(String channel)
+		{
+		Vector<Integer> c=new Vector<Integer>();
+		
+		return c;
+		}
+
+	public Vector<Integer> getZ(String channel, int frame)
+		{
+		Vector<Integer> c=new Vector<Integer>();
+		
+		return c;
+		}
+	
+	
+	
 	
 	//get iterator: image + line iterator or pixel iterator or entire image?
 	//which channels, frames, slices are affected?
-	
 	
 	
 	
