e3d07e8a883ffbb1193e1bfa10bd46534c83d052
Johan Henriksson <mahogny@areta.org>
2007-10-12 10:55:52 +0000

    invertfilter started

---
+++ ev/evplugin/filter/FilterROI.java
@@ -0,0 +1,15 @@
+package evplugin.filter;
+
+/**
+ * Filter that works on ROI level. This includes an "image level" since it is so easy to just cut out a ROI
+ * after an entire image has been processed.
+ * 
+ * @author Johan Henriksson
+ */
+public abstract class FilterROI extends FilterMeta
+	{
+
+	
+	
+	
+	}
