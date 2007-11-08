f7a9718e66c3f337f0693c533ed9237ce819cc35
Johan Henriksson <mahogny@areta.org>
2007-11-08 10:14:59 +0000

    batchmovie

--- ev/util/BatchMovie.java
+++ ev/util/BatchMovie.java
@@ -19,7 +19,10 @@ public class BatchMovie
 		System.out.println("Converting imageset "+file.getPath());
 		OstImageset ost=new OstImageset(file.getPath());
 		Vector<CalcThread.MovieChannel> channelNames=new Vector<CalcThread.MovieChannel>();
-		channelNames.add(new CalcThread.MovieChannel("GFPmax",false));
+		ost.channelImages.containsKey("GFPmax")
+			channelNames.add(new CalcThread.MovieChannel("GFPmax",false));
+		ost.channelImages.containsKey("RFPmax")
+			channelNames.add(new CalcThread.MovieChannel("RFPmax",false));
 		channelNames.add(new CalcThread.MovieChannel("DIC",true));
 		BatchThread c=new CalcThread(ost, 0, 1000000, 35, channelNames, 336, "h.264 (MPEG-4)", "High"); 
 		new CompleteBatch(c);
@@ -34,7 +37,17 @@ public class BatchMovie
 		EV.loadPlugins();
 
 		if(arg.length==0)
-			arg=new String[]{"/Volumes/TBU_xeon01_500GB01/final_recordings"};
+			arg=new String[]{
+					"/Volumes/TBU_xeon01_500GB01/ost3dfailed/",
+					"/Volumes/TBU_xeon01_500GB01/ost3dgood/",
+					"/Volumes/TBU_xeon01_500GB01/ost4dfailed/",
+					"/Volumes/TBU_xeon01_500GB01/ost4dgood",
+					"/Volumes/TBU_xeon01_500GB02/ost3dfailed/",
+					"/Volumes/TBU_xeon01_500GB02/ost3dgood/",
+					"/Volumes/TBU_xeon01_500GB02/ost4dfailed/",
+					"/Volumes/TBU_xeon01_500GB02/ost4dgood",
+					
+		};
 		for(String s:arg)
 			for(File file:(new File(s)).listFiles())
 				if(file.isDirectory())
