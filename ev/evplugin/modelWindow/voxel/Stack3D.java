6a651c148c20f2d585582fff981fecda4b7ce492
Johan Henriksson <mahogny@areta.org>
2008-05-20 15:23:32 +0000

    stack3d planes semi

--- ev/evplugin/modelWindow/voxel/Stack3D.java
+++ ev/evplugin/modelWindow/voxel/Stack3D.java
@@ -4,11 +4,11 @@ import java.awt.*;
 import java.awt.image.*;
 import java.nio.ByteBuffer;
 import java.util.*;
-import java.util.List;
 
 import javax.media.opengl.*;
 import javax.media.opengl.glu.GLU;
 import javax.swing.SwingUtilities;
+import javax.vecmath.Vector3d;
 
 
 import evplugin.ev.Tuple;
@@ -37,14 +37,16 @@ public class Stack3D
 	
 	private static class OneSlice
 		{
-		int w, h, d; //new variable d
+		int w, h, d; //size in pixels
 		//double posZ; //start-z
 		double resX,resY,resZ;
 		Texture tex; //could be multiple textures, interleaved
 		Color color;
+		
+		double realw, realh, reald; //size in um
 		}
 	
-	
+	//Maybe merge oneslice & texture? somehow
 	private static class Texture
 		{
 		public int id;
@@ -58,64 +60,42 @@ public class Stack3D
 				this.width=width;
 				this.height=height;
 				this.depth=depth;
-				//b.pu
 				}
 			}
 		public void upload(GL gl)
 			{
 			System.out.println("size "+width+height+depth);
-			//depth=64; //temp
-			
 			
 			//b=ByteBuffer.allocate(width*height*depth);
 			b.rewind();
 			for(int k=0;k<depth;k++)
 				for(int j=0;j<height;j++)
 					for(int i=0;i<width;i++)
-						{
-						//if(i>2 && j>2 && i<10 && j<10)
+						/*
 						if((i>2 && j>2 && i<10 && j<10) || (i>500 && j>500 && i<510 && j<510))
 							b.put((byte)128);
 						else
 							b.get();
-//							b.put((byte)0);
-						//b.put((byte)(((i+j+k)/5)%128));
-						}
+							*/
+						if((i>10 && j>10 && i<510 && j<510))
+							b.put((byte)128);
+						else
+							b.put((byte)5);
 			
-			gl.glEnable( GL.GL_TEXTURE_3D ); //does it have to be on here?
-
-			System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
-
-			
-			System.out.println(""+width+" "+height+" "+depth+" "+b.position()+" "+b.remaining());
 			int ids[]=new int[1];
 			gl.glGenTextures(1, ids, 0);
 			id=ids[0];
 			bind(gl);
-//			gl.glBindTexture(GL.GL_TEXTURE_3D, id);
 
+			gl.glEnable( GL.GL_TEXTURE_3D ); //does it have to be on here?
 			gl.glTexParameteri(GL.GL_TEXTURE_3D, GL.GL_TEXTURE_MIN_FILTER, GL.GL_LINEAR);
 			gl.glTexParameteri(GL.GL_TEXTURE_3D, GL.GL_TEXTURE_MAG_FILTER, GL.GL_LINEAR);
-			
 			gl.glTexParameteri(GL.GL_TEXTURE_3D, GL.GL_TEXTURE_WRAP_S, GL.GL_CLAMP);
 			gl.glTexParameteri(GL.GL_TEXTURE_3D, GL.GL_TEXTURE_WRAP_T, GL.GL_CLAMP);
 			gl.glTexParameteri(GL.GL_TEXTURE_3D, GL.GL_TEXTURE_WRAP_R, GL.GL_CLAMP);
-			
 //			gl.glTexImage3D(GL.GL_TEXTURE_3D, 0, GL.GL_ALPHA, width, height, depth, 0, GL.GL_ALPHA, GL.GL_UNSIGNED_BYTE, b);
-System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
-
-			gl.glTexImage3D(GL.GL_TEXTURE_3D, 0, GL.GL_ALPHA, 
-					width, height, depth, 0, 
-					GL.GL_ALPHA, GL.GL_UNSIGNED_BYTE, b.rewind());
-/*			gl.glTexImage3D(GL.GL_TEXTURE_3D, 0, GL.GL_ALPHA, 
-					width, height, depth, 0, 
-					GL.GL_ALPHA, GL.GL_UNSIGNED_BYTE, b.rewind());*/
-			//gl.glTexImage3D(GL.GL_TEXTURE_3D, 0, GL.GL_RGB, 
-					//width, height, depth, 0, GL.GL_RGB, GL.GL_UNSIGNED_BYTE, b.rewind());
-			
-			
+			gl.glTexImage3D(GL.GL_TEXTURE_3D, 0, GL.GL_ALPHA, width, height, depth, 0, GL.GL_ALPHA, GL.GL_UNSIGNED_BYTE, b.rewind());
 			System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
-			
 			gl.glDisable( GL.GL_TEXTURE_3D );
 
 			}
@@ -125,12 +105,6 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 			gl.glDeleteTextures(1, texlist, 0);
 			}
 		
-		public void enable()
-			{
-			}
-		public void disable()
-			{
-			}
 		
 		public void bind(GL gl)
 			{
@@ -157,7 +131,7 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 	 */ 
 	private synchronized Vector<OneSlice> getTexSlicesFrame(double z)
 		{
-		//Put it texture into list
+		//Put texture into list
 		Vector<OneSlice> texSlicesV=texSlices.get(z);
 		if(texSlicesV==null)
 			{
@@ -223,7 +197,7 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 			
 			SwingUtilities.invokeLater(new Runnable(){public void run(){w.progress.setValue(0);}});
 			
-			//im cache safety issues
+			//im. cache safety issues
 			Collection<ChannelSelection> channels=chsel.values();
 			procList.clear();
 			int curchannum=0;
@@ -240,7 +214,6 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 				OneSlice os=null;
 				
 				
-				int picpos=0;
 				
 				int skipcount=0;
 				if(slices!=null)
@@ -259,7 +232,6 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 							SwingUtilities.invokeLater(new Runnable(){public void run(){w.progress.setValue(progressSlices+progressChan);}});
 							
 							skipcount=0;
-//							System.out.println("loading #"+i);
 							EvImage evim=slices.get(i);
 							if(!chsel.filterSeq.isIdentity())
 								evim=chsel.filterSeq.applyReturnImage(evim);
@@ -275,7 +247,7 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 								os.tex=texture;
 								os.w=bim.getWidth();
 								os.h=bim.getHeight();
-								os.d=suitablePower2(slices.size());
+								os.d=ceilPower2(slices.size());
 
 								int bw=suitablePower2(os.w);
 								os.resX/=os.w/(double)bw;
@@ -286,15 +258,22 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 
 
 								System.out.println("osd "+os.d);
-								os.resX=evim.getResX()/evim.getBinning(); //px/um
+								os.resX=evim.getResX()/evim.getBinning(); //[px/um]
 								os.resY=evim.getResY()/evim.getBinning();
 								os.resZ=chsel.im.meta.resZ;
 								
-								//TODO: compensate resZ by   (slices.lastKey()-slices.firstEntry())/slices.size() 
+								double zscale=(1+slices.lastKey()-slices.firstKey())/slices.size();
+								System.out.println("zscale "+zscale);
+								os.resZ/=zscale; //TODO: check if correct. unit?
 								//TODO: check slice2d if correct there
 								
 								os.color=chsel.color;
 								texture.allocate(os.w, os.h, os.d);
+								
+								//real size
+								os.realw=os.w/os.resX;
+								os.realh=os.h/os.resY;
+								os.reald=os.d/os.resZ;
 								}
 							
 							
@@ -308,9 +287,10 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 							g.drawImage(bim,0,0,Color.BLACK,null);
 							
 							//Convert to something suitable for texture upload?
-							DataBufferByte buf=(DataBufferByte)sim.getRaster().getDataBuffer();
+							//DataBufferByte buf=(DataBufferByte)sim.getRaster().getDataBuffer();
 							//texture.b.put(buf.getData());
 							
+							//theoretically slower but can be made safer
 								Raster ras=sim.getRaster();
 								for(int ay=0;ay<os.h;ay++)
 									for(int ax=0;ax<os.w;ax++)
@@ -320,16 +300,13 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 										//texture.b.put((byte)ras.getSample(ax, ay, 0));
 										texture.b.put((byte)pix[0]);
 										}
-				
 							
-							//use sim
 							
-//							break; //only one plane
 							
 							}
 						}
 				
-				//Add black frames up to power of 2
+				//Add black frames up to z power of 2
 				int pixToWrite=os.w*os.h*os.d-texture.b.position();
 				texture.b.put(new byte[pixToWrite], 0, 0);
 				
@@ -373,9 +350,7 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 	private static int suitablePower2(int s)
 		{
 		//An option to restrict max texture size would be good
-		if(true)return 256;
-		
-		if(s>380) return 512;
+		if(s>256) return 512;
 		else if(s>192) return 256;
 		else if(s>96) return 128;
 		else if(s>48) return 64;
@@ -385,6 +360,22 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 		else return 4;
 		}
 	
+	/**
+	 * Ceil to 2^
+	 */
+	private static int ceilPower2(int s)
+		{
+		//TODO: also larger sizes
+		if(s>512) return 1024;
+		else if(s>256) return 512;
+		else if(s>256) return 256;
+		else if(s>64) return 128;
+		else if(s>32) return 64;
+		else if(s>16) return 32;
+		else if(s>8) return 16;
+		else if(s>4) return 8;
+		else return 4;
+		}
 	
 	
 	/**
@@ -392,14 +383,18 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 	 */
 	public void render(GL gl, Camera cam)
 		{
+		//int[] planes=new int[1];		gl.glGetIntegerv(GL.GL_MAX_CLIP_PLANES, planes, 0);		System.out.println("planes "+planes[0]);
+		//6 planes on NVidia macpro
+		
+		
 		if(isBuilt())
 			{
 			//gl.glBlendFunc(GL.GL_SRC_COLOR, GL.GL_ONE_MINUS_SRC_COLOR); //used before, makes no sense with alpha
 			gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-			gl.glEnable(GL.GL_BLEND); //disabled temp
+//			gl.glEnable(GL.GL_BLEND); //disabled temp
 			gl.glDepthMask(false);
 			gl.glDisable(GL.GL_CULL_FACE);
-			gl.glEnable(GL.GL_TEXTURE_3D);
+//			gl.glEnable(GL.GL_TEXTURE_3D);
 
 			
 			for(Vector<OneSlice> osv:texSlices.values())
@@ -407,31 +402,29 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 				for(OneSlice os:osv)
 					{
 					//Select texture
-					os.tex.enable();
-					os.tex.bind(gl); //NOOPs
-
-					//Find size and position
-					double w=os.w/os.resX;
-					double h=os.h/os.resY;
-					double d=os.d/os.resZ;
-
-					for(float i=0;i<1.0;i+=0.005)
-					//float i=0.5f;
+					os.tex.bind(gl);
+/*
+					//Render all planes
+					//for(float i=0;i<1.0;i+=0.005)
+					float i=0.2f;
 						{
 						float tz=i;
-						float posz=(float)(i*d);
+						float posz=(float)(i*os.reald);
 
 						gl.glBegin(GL.GL_QUADS);
 						gl.glColor3d(os.color.getRed()/255.0, os.color.getGreen()/255.0, os.color.getBlue()/255.0);
-						gl.glTexCoord3f(os.tex.left(), os.tex.top(),    tz); gl.glVertex3d(0, 0, posz); 
-						gl.glTexCoord3f(os.tex.right(),os.tex.top(),    tz); gl.glVertex3d(w, 0, posz);
-						gl.glTexCoord3f(os.tex.right(),os.tex.bottom(), tz); gl.glVertex3d(w, h, posz);
-						gl.glTexCoord3f(os.tex.left(), os.tex.bottom(), tz); gl.glVertex3d(0, h, posz);
+						gl.glTexCoord3f(0, 0, tz); gl.glVertex3d(0,        0,        posz); 
+						gl.glTexCoord3f(1, 0, tz); gl.glVertex3d(os.realw, 0,        posz);
+						gl.glTexCoord3f(1, 1, tz); gl.glVertex3d(os.realw, os.realh, posz);
+						gl.glTexCoord3f(0, 1, tz); gl.glVertex3d(0,        os.realh, posz);
 						gl.glEnd();
 						}		
-						
-
-					os.tex.disable();
+						*/
+					System.out.println("=====================================");
+					System.out.println("=====================================");
+					System.out.println("=====================================");
+					System.out.println("=====================================");
+					renderPlane(gl, cam, os);
 					}
 				}
 
@@ -445,6 +438,208 @@ System.out.println("error "+new GLU().gluErrorString(gl.glGetError()));
 			}
 		}
 
+	private static class Plane
+		{
+		public Plane(){}
+		public Plane(double A, double B, double C, double D){this.A=A;this.B=B;this.C=C;this.D=D;}
+		//Ax+By+Cz=D
+		double A, B, C, D; 
+		}
+
+	//Lines over z
+	private static Vector3d intersectPlane1(Plane p, OneSlice os)
+		{
+		if(p.C==0) return null;
+		double z=p.D/p.C;
+		if(z<0 || z>os.reald) return null;
+		return new Vector3d(0,0,z);
+		}
+	private static Vector3d intersectPlane2(Plane p, OneSlice os)
+		{
+		if(p.C==0) return null;
+		double z=(p.D-p.A*os.realw)/p.C;
+		if(z<0 || z>os.reald) return null;
+		return new Vector3d(os.realw,0,z);
+		}
+	private static Vector3d intersectPlane3(Plane p, OneSlice os)
+		{
+		if(p.C==0) return null;
+		double z=(p.D-p.B*os.realh)/p.C;
+		if(z<0 || z>os.reald) return null;
+		return new Vector3d(0,os.realh,z);
+		}
+	private static Vector3d intersectPlane4(Plane p, OneSlice os)
+		{
+		if(p.C==0) return null;
+		double z=(p.D-p.A*os.realw-p.B*os.realh)/p.C;
+		if(z<0 || z>os.reald) return null;
+		return new Vector3d(os.realw,os.realh,z);
+		}
+	
+	//Lines over y
+	private static Vector3d intersectPlane5(Plane p, OneSlice os)
+		{
+		if(p.B==0) return null;
+		double y=(p.D)/p.B;
+		if(y<0 || y>os.realh) return null;
+		return new Vector3d(0,y,0);
+		}
+	private static Vector3d intersectPlane6(Plane p, OneSlice os)
+		{
+		if(p.B==0) return null;
+		double y=(p.D-p.A*os.realw)/p.B;
+		if(y<0 || y>os.realh) return null;
+		return new Vector3d(os.realw,y,0);
+		}
+	private static Vector3d intersectPlane7(Plane p, OneSlice os)
+		{
+		if(p.B==0) return null;
+		double y=(p.D-p.C*os.reald)/p.B;
+		if(y<0 || y>os.realh) return null;
+		return new Vector3d(0,y,os.reald);
+		}
+	private static Vector3d intersectPlane8(Plane p, OneSlice os)
+		{
+		if(p.B==0) return null;
+		double y=(p.D-p.A*os.realw-p.C*os.reald)/p.B;
+		if(y<0 || y>os.realh) return null;
+		return new Vector3d(os.realw,y,os.reald);
+		}
+
+	//Lines over z
+	private static Vector3d intersectPlane9(Plane p, OneSlice os)
+		{
+		if(p.A==0) return null;
+		double x=(p.D)/p.A;
+		if(x<0 || x>os.realw) return null;
+		return new Vector3d(x, 0,0);
+		}
+	private static Vector3d intersectPlane10(Plane p, OneSlice os)
+		{
+		if(p.A==0) return null;
+		double x=(p.D-p.C*os.reald)/p.A;
+		if(x<0 || x>os.realw) return null;
+		return new Vector3d(x, 0,os.reald);
+		}
+	private static Vector3d intersectPlane11(Plane p, OneSlice os)
+		{
+		if(p.A==0) return null;
+		double x=(p.D-p.B*os.realh)/p.A;
+		if(x<0 || x>os.realw) return null;
+		return new Vector3d(x, os.realh,0);
+		}
+	private static Vector3d intersectPlane12(Plane p, OneSlice os)
+		{
+		if(p.A==0) return null;
+		double x=(p.D-p.B*os.realh-p.C*os.reald)/p.A;
+		if(x<0 || x>os.realw) return null;
+		return new Vector3d(x, os.realh,os.reald);
+		}
+	
+	
+
+	private Vector3d[] compactPoint(Vector3d[] p, int numv)
+		{
+		int i=0;
+		Vector3d[] np=new Vector3d[numv];
+		for(Vector3d v:p)
+			if(v!=null)
+				{
+				np[i]=v;
+				i++;
+				}
+		return np;
+		}
+	private Vector3d[] invertOrder(Vector3d[] p)
+		{
+		Vector3d v=p[0];
+		p[0]=p[1];
+		p[1]=v;
+		return p;
+		}
+	
+	private void renderPlane(GL gl, Camera cam, OneSlice os)
+		{
+		
+		//Figure out how large a polygon would have to be to cut the cube for sure
+		double neededSize=os.realw;
+		if(os.realh>neededSize)
+			neededSize=os.realh;
+		if(os.reald>neededSize)
+			neededSize=os.reald;
+		neededSize*=20; //Arbitrary value, just be sure it is big
+
+//		for(float i=0;i<1.0;i+=0.005)
+		//float i=0.2f;
+		for(double q=0;q<os.realh*4;q+=os.realh/10)
+			{
+
+//			Plane p=new Plane(1,0,0,os.realw);
+//			Plane p=new Plane(0,1,0,os.realh);
+//			Plane p=new Plane(0,0,1,os.reald);
+			Plane p=new Plane(1,1,1,q);
+			
+			Vector3d[] points=new Vector3d[]{
+					intersectPlane1(p, os), intersectPlane2(p, os), intersectPlane4(p, os), intersectPlane3(p, os),
+					intersectPlane5(p, os), intersectPlane6(p, os), intersectPlane8(p, os), intersectPlane7(p, os),
+					intersectPlane9(p, os), intersectPlane10(p, os), intersectPlane12(p, os), intersectPlane11(p, os)};
+			int activelist=0;
+			int numv=0;
+			for(int ai=0;ai<12;ai++)
+				if(points[ai]!=null)
+					{
+					activelist+=1<<ai;
+					numv++;
+					}
+			
+			switch(activelist)
+				{
+				case 3168: //110001100000  ok?
+					break;
+				case 3172: //110001100100
+					break;
+				case 912: //fel
+				case 2730: //fel
+					points=invertOrder(compactPoint(points, numv));
+				break;
+				case 3002: //8 wtf
+					break;
+				}
+			
+			System.out.println("points: "+activelist+ " // "+numv);
+			
+			gl.glBegin(GL.GL_LINE_LOOP);
+//			gl.glBegin(GL.GL_POLYGON);
+			gl.glColor3d(os.color.getRed()/255.0, os.color.getGreen()/255.0, os.color.getBlue()/255.0);
+			for(Vector3d po:points)
+				if(po!=null)
+					{
+					System.out.println(po);
+					point(gl, os, po.x, po.y, po.z);
+					}
+			
+			/*
+			point(gl,os,0,0,posz);
+			point(gl,os,neededSize,0,posz);
+			point(gl,os,neededSize,neededSize,posz);
+			*/
+/*			point(gl,os,0,0,posz);
+			point(gl,os,os.realw,0,posz);
+			point(gl,os,os.realw,os.realh,posz);*/
+			gl.glEnd();
+			}
+		}
+	
+	private void point(GL gl, OneSlice os, double x, double y, double z) //spatial coordinates
+		{
+		gl.glTexCoord3f((float)x/(float)os.realw, (float)y/(float)os.realh, (float)z/(float)os.reald); gl.glVertex3d(x,y,z);
+		}
+
+	
+	
+	
+	
+	
 	
 	private boolean isBuilt()
 		{
