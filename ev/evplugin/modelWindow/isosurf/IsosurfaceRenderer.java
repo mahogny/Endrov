aebf9b12221d75d4b5fda2f566e1cb92bee56145
Johan Henriksson <mahogny@areta.org>
2008-05-29 15:31:12 +0000

    isosurf asynchost daemon bugfix

--- ev/evplugin/modelWindow/isosurf/IsosurfaceRenderer.java
+++ ev/evplugin/modelWindow/isosurf/IsosurfaceRenderer.java
@@ -1,170 +1,94 @@
 package evplugin.modelWindow.isosurf;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.ConvolveOp;
-import java.awt.image.Kernel;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
-import java.util.TreeMap;
+import java.util.Collection;
+import java.util.Collections;
+
 import javax.media.opengl.GL;
-import javax.vecmath.Vector3f;
-import com.sun.opengl.util.BufferUtil;
-import evplugin.imageset.EvImage;
-import evplugin.imageset.Imageset;
+
+import evplugin.ev.Vector3D;
+import evplugin.modelWindow.ModelWindow;
 
 
 
 //can replace float with short, half memory => twice basic speed
 //*should* load images while rendering, skipping the ptscalarstep
 
+//Use VBO to only upload once
+
+//role of generator half moved here. just need the list management. or register callbacks?
 
 /**
- * 
+ * Renderer of isosurfaces
  * @author Johan Henriksson
  */
 public class IsosurfaceRenderer
 	{
-	public Isosurface iso=new Isosurface();
-	FloatBuffer vertb;
-	FloatBuffer vertn;
-	IntBuffer indb;
-
-	public IsosurfaceRenderer(Imageset im, String channelName, int cframe, int blursize, float cutoff)
+	public FloatBuffer vertb;
+	public FloatBuffer vertn;
+	public IntBuffer indb;
+	public float maxX,maxY,maxZ,minX,minY,minZ;
+	
+	public Collection<Double> adjustScale(ModelWindow w)
 		{
-		//Prepare to blur XY
-		ConvolveOp simpleBlur=null;
-		if(blursize!=0)
-			{
-			int blurarrsize=(1+2*blursize)*(1+2*blursize);
-			float weight = 1.0f/(float)blurarrsize;
-			float[] elements = new float[blurarrsize]; 
-			for (int i = 0; i < elements.length; i++) 
-				elements[i] = weight;
-			Kernel myKernel = new Kernel(blursize*2+1, blursize*2+1, elements);
-			simpleBlur = new ConvolveOp(myKernel);
-			}
-
-		int w=0,h=0,d=0;
-		float realw=0,realh=0,reald=0; //TODO: should be able to have different distances
-
-		float ptScalarField[]=null;
-		if(im.channelImages.containsKey(channelName) &&
-				im.channelImages.get(channelName).imageLoader.containsKey(cframe))
-			{
-			double resZ=im.meta.resZ;
-
-			//long t=System.currentTimeMillis();
-
-			TreeMap<Integer,EvImage> slices=im.channelImages.get(channelName).imageLoader.get(cframe);
-			int curslice=0;
-			if(slices!=null)
-				for(int i:slices.keySet())
-					{
-					EvImage evim=slices.get(i);
-					BufferedImage bim=evim.getJavaImage();
-
-					//Blur the image
-					if(simpleBlur!=null)
-						{
-						BufferedImage bufo=new BufferedImage(bim.getWidth(), bim.getHeight(), bim.getType());
-						simpleBlur.filter(bim, bufo);
-						bim=bufo;
-						}
-
-					if(ptScalarField==null)
-						{
-						w=bim.getWidth();
-						h=bim.getHeight();
-						d=slices.size();
-						realw=(float)bim.getWidth()/(float)(evim.getResX()/evim.getBinning());
-						realh=(float)bim.getHeight()/(float)(evim.getResY()/evim.getBinning());
-						reald=(float)d/(float)resZ;
-						ptScalarField=new float[w*h*d];
-						System.out.println("alloc "+w+" "+h+" "+d);
-						}
-
-					float[] pixels=new float[bim.getWidth()];
-					for(int y=0;y<bim.getHeight();y++)
-						{
-						bim.getRaster().getPixels(0, y, bim.getWidth(), 1, pixels);
-						for(int x=0;x<bim.getWidth();x++)
-							ptScalarField[curslice*w*h+y*w+x]=pixels[x];
-
-						}
-					curslice++;
-					}
-
-			//System.out.println("loading ok:"+(System.currentTimeMillis()-t));
-			}
-
-
-		//Generate polygons
-		if(ptScalarField!=null)
-			{
-			//Smoothen Z?
-			//Generate surface
-			iso.generateSurface(ptScalarField, cutoff, w-1, h-1, d-1, realw/w, realh/h, reald/d);
-
-			Vector3f[] vertices=iso.getVertices();
-			Vector3f[] normals=iso.getNormals();
-			int[] indices=iso.getIndices();
-			if(vertices.length>0 && indices.length>0 && normals.length>0)
-				{
-				vertb=BufferUtil.newFloatBuffer(vertices.length*3);
-				for(int i=0;i<vertices.length;i++)
-					{
-					vertb.put(vertices[i].x);
-					vertb.put(vertices[i].y);
-					vertb.put(vertices[i].z);
-					}
-				vertn=BufferUtil.newFloatBuffer(normals.length*3);
-				for(int i=0;i<normals.length;i++)
-					{
-					vertn.put(normals[i].x);
-					vertn.put(normals[i].y);
-					vertn.put(normals[i].z);
-					}
-				indb=BufferUtil.newIntBuffer(indices.length); 
-				for(int i:indices)
-					indb.put(i);
-				}
-			}
+		return Collections.singleton((double)(maxX-minX));
+		}
+	
+	/**
+	 * Give suitable center of all objects
+	 */
+	public Vector3D autoCenterMid()
+		{
+		return new Vector3D((maxX+minX)/2,(maxY+minY)/2,(maxZ+minZ)/2);
 		}
 	
 	
+	/**
+	 * Given a middle position, figure out radius required to fit objects
+	 */
+	public Double autoCenterRadius(Vector3D mid, double FOV)
+		{
+		double[] list={Math.abs(minX-mid.x),Math.abs(minY-mid.y),Math.abs(minZ-mid.z),
+				Math.abs(maxX-mid.x), Math.abs(maxY-mid.y), Math.abs(maxZ-mid.z)};
+		double max=list[0];
+		for(double d:list)
+			if(d>max)
+				max=d;
+		//Find how far away the camera has to be. really have FOV in here?
+		return max/Math.sin(FOV);
+		}
 	
+	
+	/**
+	 * Render surface
+	 */
 	public void render(GL gl, float red, float green, float blue, float trans)
 		{
-		//Render surface
 		if(vertb!=null)
 			{
 			gl.glPushAttrib(GL.GL_ALL_ATTRIB_BITS); //bother to refine?
-			
+	
 			float lightDiffuse[]=new float[]{1f, 1f, 1f, 0};
 			float lightAmbient[] = { 0.3f, 0.3f, 0.3f, 0 }; 
 			gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, lightAmbient, 0);   
-	    gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, lightDiffuse, 0);
-	    //GL_LIGHT_MODEL_TWO_SIDE false right now
-			
+			gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, lightDiffuse, 0);
+			//GL_LIGHT_MODEL_TWO_SIDE false right now
+	
 			boolean doTransparent=trans<0.99;
-	    
-			
-	    
-	    if(doTransparent)
-	    	{
-	    	//NEHE does additive instead
-	    	
-		    gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+	
+			if(doTransparent)
+				{
+				//NEHE does additive instead
+				gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
 				gl.glEnable(GL.GL_BLEND);
 				gl.glDepthMask(false);
 				gl.glDisable(GL.GL_CULL_FACE);
-				
+	
 				gl.glColorMaterial ( GL.GL_FRONT_AND_BACK, GL.GL_AMBIENT_AND_DIFFUSE ) ;
 				gl.glEnable ( GL.GL_COLOR_MATERIAL ) ;
-				
-	    	}
-				
-	    
+				}
+	
 			vertb.rewind();
 			vertn.rewind();
 			indb.rewind();
@@ -175,32 +99,10 @@ public class IsosurfaceRenderer
 			gl.glEnable(GL.GL_LIGHT0);
 			gl.glEnable ( GL.GL_COLOR_MATERIAL ) ;
 			gl.glColor4f(red,green,blue,trans);
-
-			
+	
 			gl.glVertexPointer(3, GL.GL_FLOAT, 0, vertb);
 			gl.glNormalPointer(GL.GL_FLOAT, 0, vertn);
 			gl.glDrawElements(GL.GL_TRIANGLES, indb.remaining(), GL.GL_UNSIGNED_INT, indb);
-
-/*
-			gl.glDisable(GL.GL_LIGHT0);
-			gl.glDisable(GL.GL_LIGHTING);
-			gl.glEnable(GL.GL_CULL_FACE);
-			gl.glDisable( GL.GL_COLOR_MATERIAL ) ;
-			gl.glDisableClientState( GL.GL_VERTEX_ARRAY );
-			gl.glDisableClientState( GL.GL_NORMAL_ARRAY );
-			
-			
-	    if(doTransparent)
-	    	{
-	    	gl.glDisable(GL.GL_BLEND);
-				gl.glDepthMask(true);
-				gl.glEnable(GL.GL_CULL_FACE);
-				
-				
-				//Can draw an additional time, but just write to Z-buffer. dunno what is best
-				
-	    	}
-	    	*/
 			}
 		gl.glPopAttrib();
 		}
