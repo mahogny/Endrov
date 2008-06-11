bda015d4f737b47eb94af737cb627e0ad38a1c40
Johan Henriksson <mahogny@areta.org>
2008-06-11 11:42:39 +0000

    imserv stuff

---
+++ ev/imserv/rmi.java
@@ -0,0 +1,10 @@
+package imserv;
+
+//http://java.sun.com/j2se/1.5.0/docs/api/javax/rmi/ssl/package-summary.html
+//http://java.sun.com/j2se/1.5.0/docs/guide/rmi/socketfactory/SSLInfo.html
+//http://java.sun.com/j2se/1.5.0/docs/guide/security/jsse/samples/index.html
+
+public class rmi
+	{
+
+	}
