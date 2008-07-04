26536ff671f0a551de54f80ec8e2774e07f5141e
Johan Henriksson <mahogny@areta.org>
2008-07-04 15:16:47 +0000

    voronoi

---
+++ qhullj/qhull/QHull.java
@@ -0,0 +1,24 @@
+package qhull;
+
+import javax.vecmath.Vector3d;
+
+public class QHull
+	{
+	private static String lock="foo";
+
+	static
+		{
+		System.loadLibrary("qhulllib");
+		}
+
+	public static boolean voronoi(VoronoiResult result, Vector3d[] points)
+		{
+		synchronized (lock)
+			{
+			return voronoi_(result, points);
+			}
+		}
+	
+	private static native boolean voronoi_(VoronoiResult result, Vector3d[] points);
+
+	}
