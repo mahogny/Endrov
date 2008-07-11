32e0d3594805e5f7c7bf15af380f12f44f86ea23
Johan Henriksson <mahogny@areta.org>
2008-07-11 10:07:41 +0000

    qhull ppc

---
+++ ev/qhull/compile.sh
@@ -0,0 +1,4 @@
+gcc stat.c io.c geom2.c poly2.c merge.c  qhull.c geom.c poly.c qvoronoi.c qset.c global.c mem.c user.c -arch i386 -arch ppc -o new/qvoronoi
+gcc stat.c io.c geom2.c poly2.c merge.c  qhull.c geom.c poly.c qset.c global.c mem.c user.c -arch i386 -arch ppc  qdelaun.c -o new/qdelaunay
+gcc -arch i386 -arch ppc  rbox.c -o new/rbox
+
