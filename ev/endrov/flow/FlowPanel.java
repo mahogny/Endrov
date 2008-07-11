cdde3429e7028cecd4001134862a4a15f486a8be
Johan Henriksson <mahogny@areta.org>
2008-07-11 15:31:54 +0000

    flow cont

--- ev/endrov/flow/FlowPanel.java
+++ ev/endrov/flow/FlowPanel.java
@@ -2,10 +2,13 @@ package endrov.flow;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.*;
+import java.util.Vector;
 
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
-public class FlowPanel extends JPanel
+public class FlowPanel extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener//, KeyListener
 	{
 	static final long serialVersionUID=0;
 
@@ -14,7 +17,12 @@ public class FlowPanel extends JPanel
 	
 	public FlowPanel()
 		{
-		
+		addMouseMotionListener(this);
+		addMouseListener(this);
+		addMouseWheelListener(this);
+/*		setEnabled(true);
+		setFocusable(true);
+		addKeyListener(this);*/
 		}
 
 
@@ -23,8 +31,74 @@ public class FlowPanel extends JPanel
 		g.setColor(Color.WHITE);
 		g.fillRect(0,0,getWidth(),getHeight());
 		
+		Vector<FlowUnit> units=new Vector<FlowUnit>();
+		units.add(new FlowUnit());
+		
+		for(FlowUnit u:units)
+			u.paint(g, this);
+		
 		
 		}
+	
+	
+	private int mouseLastDragX=0, mouseLastDragY=0;
+//	public int mouseLastMoveX, mouseLastMoveY;
+
+
+	public void mouseWheelMoved(MouseWheelEvent e){}
+
+	public void mouseDragged(MouseEvent e)
+		{
+		int dx=(e.getX()-mouseLastDragX);
+		int dy=(e.getY()-mouseLastDragY);
+		if(SwingUtilities.isRightMouseButton(e))
+			{
+			cameraX-=dx;
+			cameraY-=dy;
+			repaint();
+			}
+		
+		
+		mouseLastDragX=e.getX();
+		mouseLastDragY=e.getY();
+		}
+
+
+	public void mouseMoved(MouseEvent e)
+		{
+//		int sdx=(e.getX()-mouseLastMoveX);
+//		int sdy=(e.getY()-mouseLastMoveY);
+	//	mouseLastMoveX=e.getX();
+	//	mouseLastMoveY=e.getY();
+		}
+
+/*
+	public void keyPressed(KeyEvent e){}
+
+
+	public void keyReleased(KeyEvent e){}
+
+
+	public void keyTyped(KeyEvent e){}
+*/
+
+	public void mouseClicked(MouseEvent e){}
+
+
+	public void mouseEntered(MouseEvent e){}
+
+
+	public void mouseExited(MouseEvent e){}
+
+
+	public void mousePressed(MouseEvent e)
+		{
+		mouseLastDragX=e.getX();
+		mouseLastDragY=e.getY();
+		}
+
+
+	public void mouseReleased(MouseEvent e){}
 	
 	
 	
--- ev/endrov/flow/FlowUnit.java
+++ ev/endrov/flow/FlowUnit.java
@@ -1,6 +1,28 @@
 package endrov.flow;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
 public class FlowUnit
 	{
-
+	int x,y;
+	
+	
+	public Dimension getBoundingBox()
+		{
+		Dimension d=new Dimension(30,30);
+		return d;
+		}
+	
+	
+	public void paint(Graphics g, FlowPanel panel)
+		{
+		g.setColor(Color.blue);
+		g.drawRect(x-panel.cameraX,y-panel.cameraY,30,30);
+		
+		
+		
+		}
+	
 	}
