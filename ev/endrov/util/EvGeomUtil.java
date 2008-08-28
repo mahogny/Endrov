172eb37e7446c100b1db92fc0071a5b3b474441e
Johan Henriksson <mahogny@areta.org>
2008-08-28 16:11:17 +0000

    ccm1

--- ev/endrov/util/EvGeomUtil.java
+++ ev/endrov/util/EvGeomUtil.java
@@ -1,6 +1,11 @@
 package endrov.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import javax.vecmath.Vector3d;
+
 
 public class EvGeomUtil
 	{
@@ -35,5 +40,63 @@ public class EvGeomUtil
 		Vector3d bc=new Vector3d(posC);
 		bc.sub(posB);
 		return Math.acos(ba.dot(bc)/(ba.length()*bc.length()));
+		}
+	
+	
+	
+	
+	
+	
+	/**
+	 * Internal class used to find special cases of cube cuts
+	 */
+	private static class UnclassifiedPoint implements Comparable<UnclassifiedPoint>
+		{
+		public Vector3d v;
+		public double angle;
+		public int compareTo(UnclassifiedPoint o)
+			{
+			if(angle<o.angle)				return -1;
+			else if(angle>o.angle)	return 1;
+			else										return 0;
+			}
+		}
+
+	/**
+	 * Sorts vertices in a convex polygon
+	 */
+	public static Vector3d[] sortConvexPolygon(Vector3d[] points)
+		{
+		Vector3d center=new Vector3d();
+		for(Vector3d v:points)
+			center.add(v);
+		center.scale(1.0/points.length);
+
+		//Project points
+		List<UnclassifiedPoint> ups=new ArrayList<UnclassifiedPoint>();
+		for(int ap=0;ap<points.length;ap++)
+			if(points[ap]!=null)
+				{
+				UnclassifiedPoint up=new UnclassifiedPoint();
+				up.v=points[ap];
+				Vector3d v=new Vector3d(points[ap]);
+				v.sub(center);
+				up.angle=Math.atan2(v.y, v.x); 
+				//This *will* explode in some instances. a rotation would solve it.
+				ups.add(up);
+				}
+		Collections.sort(ups);
+		
+		for(int ap=0;ap<points.length;ap++)
+			points[ap]=ups.get(ap).v;
+		return points;
+		}
+	
+	public static void main(String[] arg)
+		{
+		Vector3d[] v=new Vector3d[]{new Vector3d(0,0,0), new Vector3d(1,0,0),
+				new Vector3d(4,1,0),new Vector3d(0,1,0)};
+		double area=polygonArea(sortConvexPolygon(v));
+		System.out.println(area);
 		}
 	}
