3b17da0ec1ecbd86797a18cca570abdffb8ebb61
Johan Henriksson <mahogny@areta.org>
2008-09-02 13:29:25 +0000

    numneigh

---
+++ ev/util2/cellContactMap/plotnumneigh.m
@@ -0,0 +1,15 @@
+dat=load('/Volumes/TBU_main02/ost4dgood/celegans2008.2.ost/data/numneigh.txt')
+numcontact=dat(:,2);
+
+dat2=load('/Volumes/TBU_main02/ost4dgood/stdcelegansNew.ost/data/henriksson/volstats.txt');
+curframe=dat2(:,1); %only ok frames
+numcell=dat2(:,2);
+
+numcontact=numcontact(1:length(curframe));
+
+
+
+
+plot(numcontact./numcell)
+
+%jurgen cuts y. only show 3+ contacts?
