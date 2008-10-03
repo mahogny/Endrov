4b746f5bd8cd6b8854ffca0f1257efb8d3d149e0
Johan Henriksson <mahogny@areta.org>
2008-10-03 09:34:03 +0000

    annot

---
+++ ev/bioserv/netio/NetFunc.java
@@ -0,0 +1,19 @@
+package bioserv.netio;
+
+public @interface NetFunc
+	{
+	String name(); //default "[unassigned]"; 
+
+	
+	}
+
+
+/*
+@RequestForEnhancement(
+    id       = 2868724,
+    synopsis = "Enable time-travel",
+    engineer = "Mr. Peabody",
+    date     = "4/1/3007"
+)
+public static void travelThroughTime(Date destination) { ... }
+*/
