3d7bcb6806e11497db1174b35056efe86c8917ff
Johan Henriksson <mahogny@areta.org>
2008-10-13 16:02:39 +0000

    rmi

--- ev/bioserv/netio/Connection.java
+++ ev/bioserv/netio/Connection.java
@@ -1,7 +1,10 @@
 package bioserv.netio;
 
 import java.io.IOException;
+import java.util.LinkedList;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 
@@ -16,10 +19,17 @@ public class Connection
 	
 		SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
 		SSLSocket socket = (SSLSocket)factory.createSocket(host, port);
+		
+		
+		
+//		socket.startHandshake();
+	//	java.security.cert.Certificate[] serverCerts = socket.getSession().getPeerCertificates();
+
+		
 		//Can disable encryption if wanted. both sides need to agree in that case
 		
 	//user/pass auth possible here
-		
+		//can be done with RMI
 		
 		
 		
