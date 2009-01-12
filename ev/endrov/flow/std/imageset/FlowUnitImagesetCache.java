b6254f564f72e1793221746dcbe39950debfb6fa
Johan Henriksson <mahogny@areta.org>
2009-01-12 08:44:26 +0000

    fu

---
+++ ev/endrov/flow/std/imageset/FlowUnitImagesetCache.java
@@ -0,0 +1,12 @@
+package endrov.flow.std.imageset;
+
+/**
+ * Enforce images to be cached until changed. To be applied on particularly expensive operations.
+ * 
+ * @author Johan Henriksson
+ *
+ */
+public class FlowUnitImagesetCache
+	{
+
+	}
