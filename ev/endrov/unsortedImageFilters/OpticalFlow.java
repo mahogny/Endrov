c5b0926a8cd9c41109e827d56c83d28b85df36d9
Johan Henriksson <mahogny@areta.org>
2009-04-30 07:35:29 +0000

    optical flow paper

--- ev/endrov/unsortedImageFilters/OpticalFlow.java
+++ ev/endrov/unsortedImageFilters/OpticalFlow.java
@@ -13,4 +13,19 @@ public class OpticalFlow
 	
 	//TrackEM2
 	
+	/*
+	 * 
+	 * best algorithm:
+	 * 
+	 * An I t e r a t i v e Image R e g i s t r a t i o n T e c h n i q u e
+   w i t h an A p p l i c a t i o n to Stereo Vision
+                             Bruce D. Lucas
+                              Takeo Kanade
+                    Computer Science Department
+                       Carnegie-Mellon  University
+                    Pittsburgh, Pennsylvania 15213
+
+	 * 
+	 */
+	
 	}
