b248c40c2ca062b61150cef9a5a59139503c8059
Johan Henriksson <mahogny@areta.org>
2009-05-04 18:41:50 +0000

    rec res

--- ev/endrov/recording/RecordingResource.java
+++ ev/endrov/recording/RecordingResource.java
@@ -6,4 +6,11 @@ import endrov.util.EvSound;
 public class RecordingResource
 	{
 	public static EvSound soundCameraSnap=new EvSound(RecordingResource.class,"13658__LS__camera_click.wav");
+	
+	//This forces loading of static values to be done at startup
+	public static void initPlugin() {}
+	static
+		{
+		
+		}
 	}
