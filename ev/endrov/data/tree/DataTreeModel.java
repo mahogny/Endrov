8e5c27a26741e62cff59831af2721c5e69087ce8
Johan Henriksson <mahogny@areta.org>
2009-05-14 08:40:28 +0000

    data tree

---
+++ ev/endrov/data/tree/DataTreeModel.java
@@ -0,0 +1,90 @@
+package endrov.data.tree;
+
+import java.util.LinkedList;
+
+import javax.swing.event.TreeModelListener;
+import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreePath;
+
+import endrov.data.EvContainer;
+import endrov.data.EvData;
+import endrov.data.EvObject;
+
+
+public class DataTreeModel implements TreeModel
+	{
+	LinkedList<TreeModelListener> listener=new LinkedList<TreeModelListener>(); 
+	
+	//needed?
+	//public WeakHashMap<Object, ROITreeElement> allElements=new WeakHashMap<Object, ROITreeElement>(); 
+	
+	DataTreeElement root=new DataTreeElement();
+	
+	public void addTreeModelListener(TreeModelListener l)
+		{
+		listener.add(l);
+		}
+
+	public Object getChild(Object parent, int index)
+		{
+		DataTreeElement eparent=(DataTreeElement)parent;
+		//System.out.println("+++++++++++++++++++++++++++++++++++++++++get child "+eparent.getChild(index));
+		return eparent.getChild(index);
+		}
+
+	public int getChildCount(Object parent)
+		{
+		DataTreeElement eparent=(DataTreeElement)parent;
+		if(eparent.isRoot)
+			return EvData.openedData.size();
+		else
+			return eparent.getLeaf().metaObject.size();
+		}
+
+	public int getIndexOfChild(Object parent, Object child)
+		{
+		DataTreeElement eparent=(DataTreeElement)parent;
+		
+		if(eparent.isRoot)
+			{
+			return EvData.openedData.indexOf(child);
+			}
+		else
+			{
+			EvContainer c=eparent.getLeaf();
+			int i=0;
+			for(EvObject o:c.metaObject.values())
+				{
+				if(o==child)
+					return i;
+				i++;
+				}
+			return -1;
+			}
+		}
+
+	public Object getRoot()
+		{
+		return root;
+		}
+
+	public boolean isLeaf(Object node)
+		{
+		DataTreeElement eparent=(DataTreeElement)node;
+		
+		if(eparent.isRoot)
+			return false;
+		else
+			return eparent.getLeaf().metaObject.isEmpty();
+		}
+
+	public void removeTreeModelListener(TreeModelListener l)
+		{
+		listener.remove(l);
+		}
+
+	public void valueForPathChanged(TreePath path, Object newValue)
+		{
+		}
+
+	}
