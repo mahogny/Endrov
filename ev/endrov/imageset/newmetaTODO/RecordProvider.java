6d607a391761f4c9929fa6d3883aa7f35808e32f
Johan Henriksson <mahogny@areta.org>
2009-08-10 07:13:43 +0000

    planning new OST format

---
+++ ev/endrov/imageset/newmetaTODO/RecordProvider.java
@@ -0,0 +1,18 @@
+package endrov.imageset.newmetaTODO;
+
+/**
+ * how to abstract away OMERO?
+ * provide a way to look up IDs. in future, can even provide remote reader of records
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Johan Henriksson
+ *
+ */
+public class RecordProvider
+	{
+
+	}
