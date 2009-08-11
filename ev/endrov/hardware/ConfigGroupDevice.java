7daad45b01c988228c65b67f8a41683fd9211a16
Johan Henriksson <mahogny@areta.org>
2009-08-11 08:10:56 +0000

    metadata planning. configgroupdevice

---
+++ ev/endrov/hardware/ConfigGroupDevice.java
@@ -0,0 +1,130 @@
+package endrov.hardware;
+
+import java.util.*;
+
+import endrov.recording.HWState;
+
+/**
+ * Virtual device that sets properties for other devices
+ * @author Johan Henriksson
+ *
+ */
+public class ConfigGroupDevice implements Device, HWState
+	{
+	
+	public int currentState=0;
+
+	//Name -> Option
+	public final List<Option> options=new ArrayList<Option>();
+	
+	/**
+	 * One option group
+	 * @author Johan Henriksson
+	 *
+	 */
+	public static class Option
+		{
+		//Name of option
+		public String name;
+		
+		//Device -> Property -> Value
+		public Map<DevicePath,Map<String,String>> settings=new TreeMap<DevicePath, Map<String,String>>();
+		}
+
+	public String getDescName()
+		{
+		return "ConfigGroup";
+		}
+
+	public SortedMap<String, String> getPropertyMap()
+		{
+		return new TreeMap<String,String>();
+		}
+
+	public SortedMap<String, PropertyType> getPropertyTypes()
+		{
+		return new TreeMap<String,PropertyType>();
+		}
+
+	public String getPropertyValue(String prop)
+		{
+		return null;
+		}
+
+	public Boolean getPropertyValueBoolean(String prop)
+		{
+		return null;
+		}
+
+	public void setPropertyValue(String prop, String value)
+		{
+		}
+
+	public void setPropertyValue(String prop, boolean value)
+		{
+		}
+
+	
+	
+	public int getCurrentState()
+		{
+		return currentState;
+		}
+
+	public String getCurrentStateLabel()
+		{
+		Option o=options.get(currentState);
+		if(o!=null)
+			return o.name;
+		else
+			return "";
+		}
+
+	public List<String> getStateNames()
+		{
+		LinkedList<String> names=new LinkedList<String>();
+		for(Option o:options)
+			names.add(o.name);
+		return names;
+		}
+
+	
+	
+	
+	public void setCurrentState(int state)
+		{
+		Option op=options.get(state);
+		if(op==null)
+			System.out.println("Could not find state #"+state);
+		else
+			{
+			//TODO
+			}
+		}
+
+	public void setCurrentStateLabel(String label)
+		{
+		for(int i=0;i<options.size();i++)
+			{
+			if(options.get(i).equals(label))
+				{
+				setCurrentState(i);
+				return;
+				}
+			}
+		System.out.println("Could not find state "+label);
+		}
+
+	public double getResMagX()
+		{
+		return 1;
+		}
+
+	public double getResMagY()
+		{
+		return 1;
+		}
+
+	
+
+	}
