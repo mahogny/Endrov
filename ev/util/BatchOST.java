4a6eb7e0d3655ff3970aa57604897cac3bc787c3
Johan Henriksson <mahogny@areta.org>
2009-08-18 21:11:31 +0000

    batch only disk 1

--- ev/util/BatchOST.java
+++ ev/util/BatchOST.java
@@ -28,7 +28,7 @@ public class BatchOST
 		arg=new String[]{
 				"/Volumes/TBU_main01/ost3dfailed/","/Volumes/TBU_main01/ost4dfailed/",
 				"/Volumes/TBU_main01/ost3dgood/","/Volumes/TBU_main01/ost4dgood",
-
+/*
 				"/Volumes/TBU_main02/ost3dfailed/","/Volumes/TBU_main02/ost4dfailed/",
 				"/Volumes/TBU_main02/ost3dgood/","/Volumes/TBU_main02/ost4dgood",
 
@@ -38,6 +38,7 @@ public class BatchOST
 
 				"/Volumes/TBU_main04/ost3dfailed/","/Volumes/TBU_main04/ost4dfailed/",
 				"/Volumes/TBU_main04/ost3dgood/","/Volumes/TBU_main04/ost4dgood",
+				*/
 			};
 		for(String s:arg)
 			for(File file:(new File(s)).listFiles())
