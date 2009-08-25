1752d53989f7e999e8db41a4f5dcb0167643bad7
Johan Henriksson <mahogny@areta.org>
2009-08-25 13:05:23 +0000

    batch movie

--- ev/util/BatchMovie.java
+++ ev/util/BatchMovie.java
@@ -1,12 +1,26 @@
 package util;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+import endrov.data.EvData;
+import endrov.ev.BatchThread;
+import endrov.ev.CompleteBatch;
+import endrov.ev.EV;
+import endrov.ev.EvLog;
+import endrov.ev.EvLogStdout;
+import endrov.imageset.Imageset;
+import endrov.makeMovie.EvMovieMakerFactory;
+import endrov.makeMovie.MakeMovieThread;
+import endrov.util.EvDecimal;
+
 /**
- * Go through all imagesets in a directory and run the MakeQT plugin
+ * Go through all imagesets in a directory and make movies
  * @author Johan Henriksson
  */
 public class BatchMovie
 	{
-	/*
+	
 	public static boolean first=true;
 	public static String getchdesc()
 		{
@@ -17,35 +31,44 @@ public class BatchMovie
 	
 	public static void makeMovie(File file)
 		{
-		first=true;
-		System.out.println("Imageset "+file.getPath());
-		long currentTime=System.currentTimeMillis();
-		OstImageset ost=new OstImageset(file);
-		System.out.println(" timeX "+(System.currentTimeMillis()-currentTime));
+		//first=true;
+		EvData ost=EvData.loadFile(file);
+
+		if(ost==null)
+			return;
+		else
+			{
 		
-		FilterSeq fsNull=new FilterSeq();
-		FilterSeq fsDIC=new FilterSeq();
-		
-		FilterSlice filterCB=new ContrastBrightnessFilter();
-		fsDIC.addFilter(filterCB);
-		
-		Vector<CalcThread.MovieChannel> channelNames=new Vector<CalcThread.MovieChannel>();
-		if(ost.channelImages.containsKey("GFPmax"))
-			channelNames.add(new CalcThread.MovieChannel("GFPmax",fsNull,getchdesc()));
-		if(ost.channelImages.containsKey("RFPmax"))
-			channelNames.add(new CalcThread.MovieChannel("RFPmax",fsNull,getchdesc()));
-		channelNames.add(new CalcThread.MovieChannel("DIC",fsDIC,getchdesc()));
-		System.out.println("Now making movie");
-		BatchThread c=new CalcThread(ost, 0, 1000000, 35, channelNames, 336, "h.264 (MPEG-4)", "High"); 
-		new CompleteBatch(c); //bad to have everything in constructor
-		System.out.println("Movie done");
+			System.out.println("Imageset "+file.getPath());
+			
+			List<MakeMovieThread.MovieChannel> channelNames=new LinkedList<MakeMovieThread.MovieChannel>();
+			
+			
+			Imageset imset=ost.getIdObjectsRecursive(Imageset.class).values().iterator().next();
+	
+			for(String name:new String[]{"GFPmax","ch0","DIC"})
+				if(imset.metaObject.containsKey(name))
+					channelNames.add(new MakeMovieThread.MovieChannel(name,""));
+			
+			System.out.println("Now making movie");
+			
+			BatchThread c=new MakeMovieThread(imset, EvDecimal.ZERO, new EvDecimal("1000000"), 15, 
+					channelNames, 336, "Maximum", new File(file.getParent(),file.getName()+".mov"),
+					EvMovieMakerFactory.getFactory("h.264 (MPEG-4)"));
+			
+			new CompleteBatch(c); 
+			System.out.println("Movie done");
+			}
 		}
 	
 	public static void main(String[] arg)
 		{
-		Log.listeners.add(new StdoutLog());
+		EvLog.listeners.add(new EvLogStdout());
 		EV.loadPlugins();
 
+		
+		
+		
 		if(arg.length==0)
 			arg=new String[]{
 					"/Volumes/TBU_xeon01_500GB01/ost4dgood/",
@@ -62,5 +85,5 @@ public class BatchMovie
 					System.out.println(" timeY "+(System.currentTimeMillis()-currentTime));
 					}
 		}
-	*/
+	
 	}
