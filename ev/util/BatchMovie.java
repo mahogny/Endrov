6d5bdc47a11420fc1cf66f6ca3a1cd7bd1bafe67
Johan Henriksson <mahogny@areta.org>
2009-08-27 09:01:28 +0000

    movie update

--- ev/util/BatchMovie.java
+++ ev/util/BatchMovie.java
@@ -36,13 +36,13 @@ public class BatchMovie
 		{
 		//first=true;
 		EvData ost=EvData.loadFile(file);
-
+	
 		if(ost==null)
 			return;
 		else
 			{
-			
-			
+	
+	
 			EvMovieMakerFactory factory=EvMovieMakerFactory.getFactory("QT: h.264 (MPEG-4)");
 			if(factory==null)
 				{
@@ -51,38 +51,38 @@ public class BatchMovie
 					System.out.println(">"+f.getName());
 				return;
 				}
-			
+	
 			File outfile=new File(file.getParent(),file.getName()+".mov");
 			if(outfile.exists())
 				return;
-			
+	
 			System.out.println("Imageset "+file.getPath());
-			
+	
 			List<MakeMovieThread.MovieChannel> channelNames=new LinkedList<MakeMovieThread.MovieChannel>();
-			
-			
+	
+	
 			Imageset imset=ost.getIdObjectsRecursive(Imageset.class).values().iterator().next();
 	
 			int width=336;
-			
-			
+	
+	
 			for(String name:new String[]{"GFPmax","ch0","DIC"})
 				if(imset.metaObject.containsKey(name))
 					{
 					channelNames.add(new MakeMovieThread.MovieChannel(name,""));
-					
+	
 					//Get original image size
 					EvChannel ch=(EvChannel)imset.metaObject.get(name);
 					EvStack stack=ch.getFirstStack();
 					EvPixels p=stack.firstEntry().snd().getPixels();
 					width=p.getWidth();
 					}
-			
+	
 			System.out.println("Now making movie");
-			
+	
 			BatchThread c=new MakeMovieThread(imset, EvDecimal.ZERO, new EvDecimal("1000000"), 15, 
 					channelNames, width, "Maximum", outfile, factory);
-			
+	
 			new CompleteBatch(c); 
 			System.out.println("Movie done");
 			}
@@ -92,31 +92,32 @@ public class BatchMovie
 		{
 		EvLog.listeners.add(new EvLogStdout());
 		EV.loadPlugins();
-
-		
-		
-		
+	
+	
+	
+	
 		if(arg.length==0)
 			arg=new String[]{
 					"/Volumes/TBU_main01/ost4dgood/",
 					"/Volumes/TBU_main02/ost4dgood/",
 					"/Volumes/TBU_main03/ost4dgood/",
 					"/Volumes/TBU_main04/ost4dgood/",
-					
+	
 		};
 		for(String s:arg)
 			for(File file:(new File(s)).listFiles())
-				if(!file.getName().endsWith(".mov"))
-//				if(file.isDirectory())
-					{
-					System.out.println(file);
-	//				long currentTime=System.currentTimeMillis();
-					makeMovie(file);
-		//			System.out.println(" timeY "+(System.currentTimeMillis()-currentTime));
-					}
-		
+				if(!file.getName().startsWith("."))
+					if(!file.getName().endsWith(".mov"))
+						if(file.isFile())
+							{
+							System.out.println(file);
+							//				long currentTime=System.currentTimeMillis();
+							makeMovie(file);
+							//			System.out.println(" timeY "+(System.currentTimeMillis()-currentTime));
+							}
+	
 		System.exit(0);
-		
+	
 		}
 	
 	
