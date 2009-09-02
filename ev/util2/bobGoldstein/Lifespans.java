454cc941aa9e6f1f331bfa370db9fed4116f4214
Johan Henriksson <mahogny@areta.org>
2009-09-02 13:48:19 +0000

    bob goldstein: lifetime calc

---
+++ ev/util2/bobGoldstein/Lifespans.java
@@ -0,0 +1,51 @@
+package util2.bobGoldstein;
+
+import java.io.File;
+import java.io.IOException;
+
+import endrov.data.EvData;
+import endrov.ev.EV;
+import endrov.nuc.NucLineage;
+import endrov.util.EvDecimal;
+import endrov.util.EvFileUtil;
+
+public class Lifespans 
+	{
+	public static void main(String[] args)
+		{
+		EV.loadPlugins();
+		
+		try
+			{
+			System.out.println("here");
+			EvData data=EvData.loadFile(new File("/Volumes/TBU_main06/ost4dgood/celegans2008.2.ost"));
+			
+			NucLineage lin=data.getIdObjectsRecursive(NucLineage.class).values().iterator().next();
+			
+			
+			StringBuffer outputBuffer=new StringBuffer();
+			outputBuffer.append("CELL NAME\tDIV TIME\n");
+			
+			for(String nucName:lin.nuc.keySet())
+				{
+				NucLineage.Nuc nuc=lin.nuc.get(nucName);
+
+				if(!nuc.pos.isEmpty())
+					{
+					outputBuffer.append(
+							nucName+"\t"+
+							(nuc.getLastFrame().divide(new EvDecimal("60")))+"\n");
+					}
+				}
+
+			
+			EvFileUtil.writeFile(new File("/tmp/forbob.csv"), outputBuffer.toString());
+			}
+		catch (IOException e)
+			{
+			e.printStackTrace();
+			}
+		
+		System.exit(0);
+		}
+	}
