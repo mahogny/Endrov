28555baf7890b038fd71ebf85c4e6fcff33eea87
Johan Henriksson <mahogny@areta.org>
2009-09-16 09:44:51 +0200

    work on spring graphs

---
+++ ev/endrov/springGraph/Graph.java
@@ -0,0 +1,22 @@
+package endrov.springGraph;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import endrov.util.Tuple;
+
+/**
+ * 
+ * @author Johan Henriksson
+ *
+ * @param <E>
+ */
+public class Graph<E>
+	{
+	Set<E> nodes=new HashSet<E>();
+	Set<Tuple<E,E>> edges=new HashSet<Tuple<E,E>>();
+
+	//One of many possible graph structures
+	
+	}
