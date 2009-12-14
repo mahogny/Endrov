78f3439306f9840ee6c80311cce0647f263cf9d4
David Johansson <jdavid@kth.se>
2009-12-14 19:51:09 +0100

    First post!

---
+++ ev/endrov/frivolous/FrivolousCamera.java
@@ -0,0 +1,71 @@
+package endrov.frivolous;
+
+import java.util.SortedMap;
+
+import endrov.hardware.PropertyType;
+import endrov.recording.CameraImage;
+import endrov.recording.HWCamera;
+
+public class FrivolousCamera implements HWCamera {
+
+	public CameraImage snap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDescName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SortedMap<String, String> getPropertyMap() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public SortedMap<String, PropertyType> getPropertyTypes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getPropertyValue(String prop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean getPropertyValueBoolean(String prop) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean hasConfigureDialog() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void openConfigureDialog() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setPropertyValue(String prop, String value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setPropertyValue(String prop, boolean value) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public double getResMagX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getResMagY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
