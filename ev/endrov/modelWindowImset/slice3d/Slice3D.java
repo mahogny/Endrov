e1a553715b62db686d5d368f02f6a6ea537ec11f
Johan Henriksson <mahogny@areta.org>
2010-01-14 12:25:53 +0100

    auto-added license header to files. this should help downstream such as ohloh and debian to autodetect license.

--- ev/endrov/modelWindowImset/slice3d/Slice3D.java
+++ ev/endrov/modelWindowImset/slice3d/Slice3D.java
@@ -1,3 +1,8 @@
+/***
+ * Copyright (C) 2010 Johan Henriksson
+ * This code is under the Endrov / BSD license. See www.endrov.net
+ * for the full text and how to cite.
+ */
 package endrov.modelWindowImset.slice3d;
 
 import java.awt.*;
--- ev/util2/nucTracker/CollectImages2.java
+++ ev/util2/nucTracker/CollectImages2.java
@@ -1,3 +1,8 @@
+/***
+ * Copyright (C) 2010 Johan Henriksson
+ * This code is under the Endrov / BSD license. See www.endrov.net
+ * for the full text and how to cite.
+ */
 package util2.nucTracker;
 
 import java.awt.Graphics2D;
