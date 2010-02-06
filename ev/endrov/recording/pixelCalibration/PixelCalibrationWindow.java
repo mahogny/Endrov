cb9cc65ed357c941a4899a6077518a9779a1ceec
Johan Henriksson <mahogny@areta.org>
2010-02-06 16:47:37 +0100

    work on acq dialogs

--- ev/endrov/recording/pixelCalibration/PixelCalibrationWindow.java
+++ ev/endrov/recording/pixelCalibration/PixelCalibrationWindow.java
@@ -3,4 +3,26 @@ package endrov.recording.pixelCalibration;
 public class PixelCalibrationWindow
 	{
 
+	
+	/**
+	 * 
+	 * micromanager:
+	 * 
+	 * new
+	 * edit
+	 * remove
+	 * remove all
+	 * 
+	 * 
+	 * ***
+	 * 
+	 * table	 label, pixel size [um]
+	 * 
+	 * 
+	 * then group config. for each property, setting and [use]
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
 	}
