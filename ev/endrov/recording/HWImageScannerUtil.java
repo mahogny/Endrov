0bda855023c18f73489ebe8bc2e47327aa5bbb81
Johan Henriksson <mahogny@areta.org>
2010-05-07 19:45:02 +0200

    start on confocal support

---
+++ ev/endrov/recording/HWImageScannerUtil.java
@@ -0,0 +1,24 @@
+package endrov.recording;
+
+import endrov.roi.ROI;
+
+
+/**
+ * PMTs and scanning mirror, whatever needed to run a scanning confocal that is not covered elsewhere.
+ * 
+ * Can emulate a camera if the additional features are not needed
+ * 
+ * @author Johan Henriksson
+ */
+public class HWImageScannerUtil 
+	{
+	
+	/*
+	public int[] makeROI(HWImageScanner scanner, ROI roi, double stageX, double stageY)
+		{
+		
+		}
+*/
+	
+	//public static void foo(){}
+	}
