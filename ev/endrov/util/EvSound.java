71731c8c9ed0c1458783822e6415787d056cfb05
Johan Henriksson <mahogny@areta.org>
2010-08-13 18:30:58 +0200

    removed mp3 and wav support since these doesn't work very well (jlayer is slow, wav not portable). added ogg support. superposed model for expression integration

--- ev/endrov/util/EvSound.java
+++ ev/endrov/util/EvSound.java
@@ -5,16 +5,6 @@
  */
 package endrov.util;
 
-import java.io.*;
-import java.net.URL;
-import javax.sound.sampled.*;
-import javax.swing.*;
-
-import endrov.ev.EvLog;
-
-import javazoom.jl.decoder.JavaLayerException;
-import javazoom.jl.player.Player;
-
 /**
  * Quick routines for playing sound. Wraps around other libraries.
  * @author Johan Henriksson
@@ -22,62 +12,16 @@ import javazoom.jl.player.Player;
  */
 public class EvSound
 	{
-	Clip clip=null;
-	Player p=null;
+	private Class<?> cl=null;
+	private String name;
 	
 	public EvSound(Class<?> cl, String name)
 		{
-		URL url=cl.getResource(name);
-		if(name.endsWith(".mp3"))
-			{
-			//TODO only works once. slow?
-      try
-				{
-				p = new Player(new BufferedInputStream(cl.getResourceAsStream(name)));
-				}
-			catch (JavaLayerException e)
-				{
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(null, "Problem loading "+url);
-				System.exit(0);
-				}
-			}
-		else
-			{
-			try 
-				{
-				// Open an audio input stream.
-				AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
-				// Get a sound clip resource.
-				clip = AudioSystem.getClip();
-				// Open audio clip and load samples from the audio input stream.
-				clip.open(audioIn);
-				} 
-			catch (UnsupportedAudioFileException e) 
-				{
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(null, "Problem loading "+url);
-				System.exit(0);
-				} 
-			catch (IOException e) 
-				{
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(null, "Problem loading "+url);
-				System.exit(0);
-				} 
-			catch (LineUnavailableException e) 
-				{
-				EvLog.printError("Java sound unavailable (line unavailable)", null);
-				}
-			catch(Exception e)
-				{
-				e.printStackTrace();
-				System.out.println("Failed to load sound clip (but ignoring) "+e.getMessage());
-				}
-			}
+		this.cl=cl;
+		this.name=name;
 		}
-	
-	public static Clip loadSound(URL url)
+	/*
+	private static Clip loadSound(URL url)
 		{
 		try 
 			{
@@ -109,23 +53,14 @@ public class EvSound
 			}
 		return null;
 		}
+	*/
 	
 	/**
 	 * Play sound
 	 */
 	public void start()
 		{
-		if(p!=null)
-			try
-				{
-				p.play();
-				}
-			catch (JavaLayerException e)
-				{
-				e.printStackTrace();
-				}
-		else if(clip!=null)
-			clip.start();
+		OggPlayer.play(cl.getResourceAsStream(name));
 		}
 	
 	/**
