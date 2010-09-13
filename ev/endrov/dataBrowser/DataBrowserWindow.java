583026c7c00dff55adb3ad7cc9b82ffb28d36df2
Johan Henriksson <mahogny@encke.(none)>
2010-09-13 15:10:23 +0200

    work on data browser

--- ev/endrov/dataBrowser/DataBrowserWindow.java
+++ ev/endrov/dataBrowser/DataBrowserWindow.java
@@ -5,9 +5,17 @@
  */
 package endrov.dataBrowser;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JMenuItem;
+
 import org.jdom.Element;
 
 import endrov.basicWindow.BasicWindow;
+import endrov.basicWindow.BasicWindowExtension;
+import endrov.basicWindow.BasicWindowHook;
 import endrov.data.EvData;
 
 /**
@@ -19,6 +27,14 @@ public class DataBrowserWindow extends BasicWindow
 	{
 	private static final long serialVersionUID = 1L;
 
+	
+	public DataBrowserWindow()
+		{
+		packEvWindow();
+		setTitleEvWindow("Data Browser");
+		setVisible(true);
+		}
+	
 	/**
 	 * Essentially: only the data tree. allow new operations to be registered.
 	 * either on right-click or by clicking on a button. or menu?
@@ -54,6 +70,62 @@ public class DataBrowserWindow extends BasicWindow
 	
 	
 	
+	/******************************************************************************************************
+	 * Plugin declaration
+	 *****************************************************************************************************/
+	public static void initPlugin() {}
+	static
+		{
+		BasicWindow.addBasicWindowExtension(new DataBrowserBasic());
+		/*
+		EV.personalConfigLoaders.put("consolewindow",new PersonalConfig()
+			{
+			public void loadPersonalConfig(Element e)
+				{
+				try
+					{
+					int x=e.getAttribute("x").getIntValue();
+					int y=e.getAttribute("y").getIntValue();
+					int w=e.getAttribute("w").getIntValue();
+					int h=e.getAttribute("h").getIntValue();
+					new ConsoleWindow(x,y,w,h);
+					}
+				catch (DataConversionException e1)
+					{
+					e1.printStackTrace();
+					}
+				}
+			public void savePersonalConfig(Element e){}
+			});*/
+		}
 	
+	
+	/**
+	 * Extension to BasicWindow
+	 * @author Johan Henriksson
+	 */
+	private static class DataBrowserBasic implements BasicWindowExtension
+		{
+		public void newBasicWindow(BasicWindow w)
+			{
+			w.basicWindowExtensionHook.put(this.getClass(),new Hook());
+			}
+		private class Hook implements BasicWindowHook, ActionListener
+			{
+			public void createMenus(BasicWindow w)
+				{
+				JMenuItem mi=new JMenuItem("Data Browser",new ImageIcon(getClass().getResource("tangoConsole.png")));
+				mi.addActionListener(this);
+				w.addMenuWindow(mi);
+				}
+			
+			public void actionPerformed(ActionEvent e) 
+				{
+				new DataBrowserWindow();
+				}
+			
+			public void buildMenu(BasicWindow w){}
+			}
+		}
 
 	}
