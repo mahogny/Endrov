399f15a102e4896739135126b589bc81d0ec979d
Johan Henriksson <mahogny@areta.org>
2011-09-23 03:43:44 +0200

    added script window

---
+++ ev/endrov/scriptWindow/PLUGIN.java
@@ -0,0 +1,42 @@
+/***
+ * Copyright (C) 2010 Johan Henriksson
+ * This code is under the Endrov / BSD license. See www.endrov.net
+ * for the full text and how to cite.
+ */
+package endrov.scriptWindow;
+import endrov.ev.PluginDef;
+
+public class PLUGIN extends PluginDef
+	{
+	public String getPluginName()
+		{
+		return "Script window";
+		}
+
+	public String getAuthor()
+		{
+		return "Johan Henriksson";
+		}
+	
+	public boolean systemSupported()
+		{
+		return true;
+		}
+	
+	public String cite()
+		{
+		return "";
+		}
+	
+	public String[] requires()
+		{
+		return new String[]{};
+		}
+	
+	public Class<?>[] getInitClasses()
+		{
+		return new Class[]{ScriptWindow.class};
+		}
+	
+	public boolean isDefaultEnabled(){return true;};
+	}
