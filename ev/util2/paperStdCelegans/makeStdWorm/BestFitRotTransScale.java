ba10596d8d6223ec1424030da5e6319347dec48d
Johan Henriksson <mahogny@areta.org>
2013-03-28 00:31:47 +0100

    cleanup of lineage fitting code

--- ev/util2/paperStdCelegans/makeStdWorm/BestFitRotTransScale.java
+++ ev/util2/paperStdCelegans/makeStdWorm/BestFitRotTransScale.java
@@ -13,13 +13,14 @@ import static java.lang.Math.*;
 
 
 /**
- * Multiple fitting of several lineages
+ * Overlap several lineage objects
+ * 
  * @author Johan Henriksson
  *
  */
 public class BestFitRotTransScale
 	{
-	public Map<Lineage,TransformedLineage> lininfo=new HashMap<Lineage, TransformedLineage>();
+	public Map<Lineage,TransformedLineage> transformedLineage=new HashMap<Lineage, TransformedLineage>();
 	public List<LineagePair> pair=new LinkedList<LineagePair>();
 	public Lineage refLin;
 	
@@ -34,17 +35,17 @@ public class BestFitRotTransScale
 	 */
 	public static class TransformedLineage
 		{
-		public double[] x=new double[]{0,0,0, 0,0,0, 1};
-		public double[] dx=new double[]{0,0,0, 0,0,0, 0};
+		public double[] x=new double[]{0,0,0, 0,0,0, 1,1,1};
+		public double[] dx=new double[]{0,0,0, 0,0,0, 0,0,0};
 		public Map<String,Vector3d[]> dtransform=new HashMap<String, Vector3d[]>();
-		public Map<String,Vector3d> transformed=new HashMap<String, Vector3d>();
-		public Map<String,Vector3d> untransformed=new HashMap<String, Vector3d>();
+		public Map<String,Vector3d> transformedPoint=new HashMap<String, Vector3d>();
+		public Map<String,Vector3d> originalPoint=new HashMap<String, Vector3d>();
 		public Map<String,Double> untransformedR=new HashMap<String, Double>();
 		public int numpoint;
 		
 		public synchronized void synchAddDx(double dx[])
 			{
-			for(int i=0;i<7;i++)
+			for(int i=0;i<dx.length;i++)
 				this.dx[i]+=dx[i];
 			}
 		
@@ -52,7 +53,7 @@ public class BestFitRotTransScale
 		/**
 		 * Transform all new points
 		 */
-		public Vector<Vector3d> getTransformed(Vector<Vector3d> points, double x[])
+		public Vector<Vector3d> transformPoints(Vector<Vector3d> points, double x[])
 			{
 			Vector<Vector3d> transformed=new Vector<Vector3d>();
 			Matrix3d rotx=rotx(x[4-1]);
@@ -62,11 +63,13 @@ public class BestFitRotTransScale
 			Matrix3d rotyp=rotyp(x[5-1]);
 			Matrix3d rotzp=rotzp(x[6-1]);
 			Vector3d trans=new Vector3d(x[1-1],x[2-1],x[3-1]);
-			double scale=x[7-1];
+			double scaleX=x[7-1];
+			double scaleY=x[8-1];
+			double scaleZ=x[9-1];
 			for(int i=0;i<points.size();i++)
 				{
 				Vector3d v=points.get(i);
-				Vector3d tv=BestFitRotTransScale.transform(rotx, roty, rotz, rotxp, rotyp, rotzp, trans, scale, v, -1);
+				Vector3d tv=BestFitRotTransScale.transformPointWithDiff(rotx, roty, rotz, rotxp, rotyp, rotzp, trans, scaleX, scaleY, scaleZ, v, -1);
 				transformed.add(tv);
 				}
 			return transformed;
@@ -75,11 +78,11 @@ public class BestFitRotTransScale
 		/**
 		 * Transform one point
 		 */
-		public Vector3d transform(Vector3d v, double x[])
+		public Vector3d transformPoint(Vector3d v, double x[])
 			{
 			Vector<Vector3d> vv=new Vector<Vector3d>();
 			vv.add(v);
-			vv=getTransformed(vv,x);
+			vv=transformPoints(vv,x);
 			return vv.get(0);
 			}
 		
@@ -89,30 +92,36 @@ public class BestFitRotTransScale
 		 */
 		public void update()
 			{
-			transformed.clear();
+			transformedPoint.clear();
 			dtransform.clear();
 			numpoint=0;
-			dx=new double[]{0,0,0, 0,0,0, 0};
+			dx=new double[]{0,0,0, 0,0,0, 0,0,0};
 			
 			
+			Vector3d trans=new Vector3d(x[1-1],x[2-1],x[3-1]);
 			Matrix3d rotx=rotx(x[4-1]);
 			Matrix3d roty=roty(x[5-1]);
 			Matrix3d rotz=rotz(x[6-1]);
 			Matrix3d rotxp=rotxp(x[4-1]);
 			Matrix3d rotyp=rotyp(x[5-1]);
 			Matrix3d rotzp=rotzp(x[6-1]);
-			Vector3d trans=new Vector3d(x[1-1],x[2-1],x[3-1]);
-			double scale=x[7-1];
+			double scaleX=x[7-1];
+			double scaleY=x[8-1];
+			double scaleZ=x[9-1];
 			
-			for(Map.Entry<String, Vector3d> entry:untransformed.entrySet())
+			for(Map.Entry<String, Vector3d> entry:originalPoint.entrySet())
 				{
 				Vector3d v=entry.getValue();
-				Vector3d tv=BestFitRotTransScale.transform(rotx, roty, rotz, rotxp, rotyp, rotzp, trans, scale, v, -1);
-				transformed.put(entry.getKey(), tv);
+				Vector3d tv=BestFitRotTransScale.transformPointWithDiff(rotx, roty, rotz, rotxp, rotyp, rotzp, trans, 
+						scaleX, scaleY, scaleZ, 
+						v, -1);
+				transformedPoint.put(entry.getKey(), tv);
 				
-				Vector3d[] diff=new Vector3d[7];
-				for(int diffi=1;diffi<=7;diffi++)
-					diff[diffi-1]=BestFitRotTransScale.transform(rotx, roty, rotz, rotxp, rotyp, rotzp, trans, scale, v, diffi);
+				Vector3d[] diff=new Vector3d[dx.length];
+				for(int diffIndex=1;diffIndex<=dx.length;diffIndex++)
+					diff[diffIndex-1]=BestFitRotTransScale.transformPointWithDiff(rotx, roty, rotz, rotxp, rotyp, rotzp, trans,
+							scaleX, scaleY, scaleZ, 
+							v, diffIndex);
 				dtransform.put(entry.getKey(),diff);
 				}
 			}
@@ -132,7 +141,8 @@ public class BestFitRotTransScale
 	public static class LineagePair
 		{
 		public HashMap<String,Double> commonNuc=new HashMap<String,Double>();		
-		public Lineage goalLin, newLin;
+		public Lineage goalLin;
+		public Lineage newLin;
 		
 		
 		private void addCommonNuc(String nuc, double weight)
@@ -142,25 +152,28 @@ public class BestFitRotTransScale
 		
 		/**
 		 * Sum up dx for one pair
+		 * 
+		 * dx = sum_points  sum_diffindex   2 (d/diffIndex trans) . (point in "new" - point in "goal" ) * weight
+		 * 
 		 */
-		public void sumDxOneIteration(BestFitRotTransScale parent)
+		public void sumDxOneIteration(BestFitRotTransScale fitter)
 			{
-			TransformedLineage newInfo=parent.lininfo.get(newLin);
-			TransformedLineage goalInfo=parent.lininfo.get(goalLin);
-			double newdx[]=new double[7];
-			double goaldx[]=new double[7];
+			TransformedLineage newInfo=fitter.transformedLineage.get(newLin);
+			TransformedLineage goalInfo=fitter.transformedLineage.get(goalLin);
+			double newdx[]=new double[9];
+			double goaldx[]=new double[newdx.length];
 			for(String nucname:commonNuc.keySet())
 				{
-				Vector3d tv=newInfo.transformed.get(nucname);
-				Vector3d gv=goalInfo.transformed.get(nucname);
+				Vector3d tv=newInfo.transformedPoint.get(nucname);
+				Vector3d gv=goalInfo.transformedPoint.get(nucname);
 				Vector3d diff=new Vector3d(tv);
 				diff.sub(gv);
-				parent.incNumPointEps(diff.lengthSquared());
+				fitter.incNumPointEps(diff.lengthSquared());
 				
 				double weight=commonNuc.get(nucname);
 				
 				
-				for(int j=1;j<=7;j++)
+				for(int j=1;j<=newdx.length;j++)
 					{
 					Vector3d dtrans=newInfo.dtransform.get(nucname)[j-1];
 					newdx[j-1]+=2*dtrans.dot(diff)*weight;
@@ -201,7 +214,7 @@ public class BestFitRotTransScale
 	public BestFitRotTransScale(BestFitRotTransScale bf)
 		{
 		//Shallow copy
-		lininfo.putAll(bf.lininfo);
+		transformedLineage.putAll(bf.transformedLineage);
 		pair.addAll(bf.pair);
 		}
 	
@@ -212,9 +225,9 @@ public class BestFitRotTransScale
 	 */
 	public void addLineage(Lineage lin)
 		{
-		if(!lininfo.containsKey(lin))
+		if(!transformedLineage.containsKey(lin))
 			{
-			for(Lineage lin2:lininfo.keySet())
+			for(Lineage lin2:transformedLineage.keySet())
 				{
 				LineagePair p=new LineagePair();
 				p.goalLin=lin;
@@ -222,7 +235,7 @@ public class BestFitRotTransScale
 				pair.add(p);
 				}
 			
-			lininfo.put(lin,new TransformedLineage());
+			transformedLineage.put(lin,new TransformedLineage());
 			}
 		}
 	
@@ -231,9 +244,9 @@ public class BestFitRotTransScale
 		for(LineagePair p:pair)
 			{
 			p.commonNuc.clear();
-			for(String nucname:lininfo.get(p.newLin).untransformed.keySet())
-			p.addCommonNuc(nucname,1);
-			p.commonNuc.keySet().retainAll(lininfo.get(p.goalLin).untransformed.keySet());
+			for(String nucname:transformedLineage.get(p.newLin).originalPoint.keySet())
+				p.addCommonNuc(nucname,1);
+			p.commonNuc.keySet().retainAll(transformedLineage.get(p.goalLin).originalPoint.keySet());
 			}
 		}
 	
@@ -245,7 +258,7 @@ public class BestFitRotTransScale
 		{
 		for(int i=0;i<maxit;i++)
 			{
-			doOneIteration();
+			doOneIteration(true);
 			
 			if(i%40==0)
 				System.out.println("eps "+eps);
@@ -260,11 +273,11 @@ public class BestFitRotTransScale
 	/**
 	 * Do one single iteration
 	 */
-	public void doOneIteration()
+	public void doOneIteration(boolean noScaling)
 		{
 		//Transform all points
 		eps=0;
-		for(TransformedLineage info:lininfo.values())
+		for(TransformedLineage info:transformedLineage.values())
 			info.update();
 		
 		//For every pair, calculate how to move
@@ -272,35 +285,42 @@ public class BestFitRotTransScale
 		for(LineagePair p:pair)
 			p.sumDxOneIteration(this);
 
-		//Apply move
+		//Constants, how fast to move toward the derivative
 		double lambdaT=0.1;
 		double lambdaR=0.0001;
-//		double lambdaS=0.0001;
-		double lambdaS=0;
+		double lambdaS=0.0001;
 
 		//numpoint for each info?
 
-		for(TransformedLineage info:lininfo.values())
+		//Apply move
+		for(TransformedLineage info:transformedLineage.values())
 			if(info.numpoint>0)
 				{
-				//Weigh
+				//Weigh each dx (speed of convergence)
 				for(int i=0;i<3;i++)
 					info.dx[i]*=lambdaT/info.numpoint;
 				for(int i=3;i<6;i++)
 					info.dx[i]*=lambdaR/info.numpoint;
-				for(int i=6;i<7;i++)
+				for(int i=6;i<9;i++)
 					info.dx[i]*=lambdaS/info.numpoint;
 
-				//Update transformation
-				for(int i=0;i<7;i++)
+				//Update transformation:  new x = x - x'
+				for(int i=0;i<info.x.length;i++)
 					info.x[i]-=info.dx[i];
 
-				//Restrict values
-				if(info.x[7-1]<0.01)
-					info.x[7-1]=0.01;
-				if(info.x[7-1]>100)
-					info.x[7-1]=100;
-				info.x[7-1]=1; //fix scale
+				//Restrict scaling values
+				for(int i=7;i<=9;i++)
+					{
+					double s=info.x[i-1];
+					if(s<0.01)
+						s=0.01;
+					if(s>100)
+						s=100;
+					if(noScaling)
+						s=1;
+					info.x[i-1]=s;
+					}
+				
 				}
 
 		//Restore reference rotation
@@ -329,20 +349,31 @@ public class BestFitRotTransScale
 
 	///////////////////////// general transformation ///////////////////////////////////
 	
-	public static Vector3d transform(
+	public static Vector3d transformPointWithDiff(
 			Matrix3d rotx, Matrix3d roty, Matrix3d rotz,
 			Matrix3d rotxp, Matrix3d rotyp, Matrix3d rotzp,
-			Vector3d trans,	double scale,
-			Vector3d u, int diffi)
+			Vector3d trans,	
+			double scaleX, double scaleY, double scaleZ,
+			Vector3d point, int diffi)
 		{
-		Vector3d v=new Vector3d(u);
-		if(diffi==7) ; else v.scale(scale);
+		Vector3d v=new Vector3d(point);
+		
+		//Scaling
+		if(diffi==9) ; else v.z*=scaleZ;
+		if(diffi==8) ; else v.y*=scaleY;
+		if(diffi==7) ; else v.x*=scaleX;
+		
+		//Rotation
 		if(diffi==6) rotzp.transform(v); else rotz.transform(v);
 		if(diffi==5) rotyp.transform(v); else roty.transform(v);
 		if(diffi==4) rotxp.transform(v); else rotx.transform(v);
+		
+		//Translation
 		if(diffi==1 || diffi==2 || diffi==3)
 			v=new Vector3d(0,0,0);
 		v.add(getTrans(trans,diffi));
+		
+		
 		return v;
 		}
 	public static Vector3d getTrans(Vector3d trans, int diffi)
